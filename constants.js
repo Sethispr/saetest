537352a85338ea17ceae2a1b6d33def1c561a40a
seth <117851610+Sethispr@users.noreply.github.com>
2025-09-05 13:39:33 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const RECENT_SEARCHES_KEY = "arcana_recent_searches";
+export const RENTALS_KEY = "saelogy_my_rentals";
+export const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/";
